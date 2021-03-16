938d3ab42293685a372eb0fee6bbe2b2daada634
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-03-16 17:50:19 +0200

    insert project/employee

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <a href="/php_mysql/index.php"><button>Projektai</button></a>
-<a href="/php_mysql/index2.php"><button>Darbuotojai</button></a><br>
+<a href="/php_mysql/page.php"><button>Darbuotojai</button></a><br>
 <?php
 $servername = "localhost";
 $username = "root"; 
@@ -28,8 +28,12 @@ if (!$conn) {
 
 
 
-$sql = "SELECT projektai.id,
+$sql = "SELECT 
+        
+        projektai.id,
+       
         projektai.projektas,
+        
         darbuotojai.name
 		
  FROM projektai
@@ -40,16 +44,42 @@ $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "proj id: " . $row["id"]. " - projektas: " . $row["projektas"]. " - vardas: " . $row["name"]. "<br>";
+        echo "proj id: " . $row["id"]. " - projektas: " . $row["projektas"]. " - vardas: " . $row["name"]. "";
+        print("     <button>DELETE</button><br>");
     }
 } else {
     echo "0 results";
 }
 
 
+if(isset($_POST['create'])){
+    $stmt = $conn->prepare("INSERT INTO projektai (id, projektas, darbuotojo_id) VALUES (?, ?, ?)");
+    $stmt->bind_param("isi", $id, $projektas, $darbuotojo_id);
+    $id = $_POST['fname'];
+    // if(empty($id)){
+    //     $id = MYSQLI_AUTO_INCREMENT_FLAG;}
+    $projektas = $_POST['lname'];
+    $darbuotojo_id = $_POST['darbuotojo_id'];
+    if(empty($darbuotojo_id)){
+        $darbuotojo_id =  null;
+    }
+    $stmt->execute();
+    $stmt->close();
+    header('Location: ' . $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
+    die;
+}
+
+
 
 mysqli_close($conn);
 ?>
+<br>
+<form action="" method="POST">
+            <label for="lname"></label><br>
+            <input type="TEXT" id="lname" name="lname" value="" placeholder="Projekto pavadinimas" ><br>
+            <input type="submit" name="create" value="Pridėti projektą">
+            
+        </form>
 
 
 
--- page.php
+++ page.php
@@ -9,7 +9,7 @@
 <body>
 
 <a href="/php_mysql/index.php"><button>Projektai</button></a>
-<a href="/php_mysql/index2.php"><button>Darbuotojai</button></a><br>
+<a href="/php_mysql/page.php"><button>Darbuotojai</button></a><br>
 <?php
 $servername = "localhost";
 $username = "root"; 
@@ -24,13 +24,14 @@ if (!$conn) {
 }
 
 
-$sql = "SELECT darbuotojai.id,
+$sql = "SELECT  darbuotojai.id,
         darbuotojai.name,
         projektai.projektas
         
 		
  FROM darbuotojai
-inner JOIN projektai ON darbuotojai.id = projektai.darbuotojo_id ORDER BY id ASC";
+left JOIN projektai ON darbuotojai.id = projektai.darbuotojo_id 
+ORDER BY id ASC";
 
 // $sql = "SELECT id, name, darbuotojo_id FROM projektai";
 $result = mysqli_query($conn, $sql);
@@ -43,8 +44,31 @@ if (mysqli_num_rows($result) > 0) {
     echo "0 results";
 }
 
+if(isset($_POST['create2'])){
+    $stmt = $conn->prepare("INSERT INTO darbuotojai (id, name) VALUES (?, ?)");
+    $stmt->bind_param("is", $id, $name);
+    $id = $_POST['fname'];
+    // if(empty($id)){
+    //     $id = MYSQLI_AUTO_INCREMENT_FLAG;}
+    $name = $_POST['lname'];
+    
+    
+    $stmt->execute();
+    $stmt->close();
+    header('Location: ' . $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
+    die;
+}
+
 mysqli_close($conn);
 ?>
+
+<br>
+<form action="" method="POST">
+            <label for="lname"></label><br>
+            <input type="TEXT" id="lname" name="lname" value="" placeholder="darbuotojo vardas" ><br>
+            <input type="submit" name="create2" value="Pridėti darbuotoja">
+            
+        </form>
 
 
 
